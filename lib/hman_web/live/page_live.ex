d13e7dd971a9b8ab0149704e153393028f26e72a
Luciano García Bañuelos <luciano.garcia@tec.mx>
2022-05-17 13:00:29 -0500

    All done

--- lib/hman_web/live/page_live.ex
+++ lib/hman_web/live/page_live.ex
@@ -4,24 +4,24 @@ defmodule HmanWeb.PageLive do
   @parts [:gallows, :head, :body, :rightArm, :leftArm, :rightLeg, :rightFoot, :leftLeg, :leftFoot]
 
   def mount(_params, _session, socket ) do
-    {:ok, assign(socket, :feedback, "")}
+    {:ok, assign(socket, :feedback, "") |> assign(:turns, 9)}
   end
 
   def render(assigns) do
-    Phoenix.View.render(HmanWeb.PageView, "index.html", assign(assigns, :parts, [:gallows]))
+    Phoenix.View.render(HmanWeb.PageView, "index.html", assign(assigns, :parts, Enum.take(@parts, 9 - assigns.turns)))
   end
 
   def handle_event("new-game-req", _value, socket) do
     {:ok, game} = Hman.Game.start_link(UUID.uuid1() |> String.to_atom)
     {_turns, feedback} = Hman.Game.get_feedback(game)
-    {:noreply, socket |> assign(:game, game) |> assign(:feedback, feedback)}
+    {:noreply, socket |> assign(:game, game) |> assign(:feedback, feedback) |> assign(:turns, 9)}
   end
 
   def handle_event(event, _value, socket) do
     IO.inspect(event)
     Hman.Game.submit_guess(socket.assigns.game, event)
-    {_turns, feedback} = Hman.Game.get_feedback(socket.assigns.game)
-    {:noreply, socket |> assign(:feedback, feedback)}
+    {turns, feedback} = Hman.Game.get_feedback(socket.assigns.game)
+    {:noreply, socket |> assign(:feedback, feedback) |> assign(:turns, turns)}
   end
 
 end
