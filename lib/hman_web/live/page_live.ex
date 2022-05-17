51fc9faa0a4489fbe473adb427c8942446e200d9
Luciano García Bañuelos <luciano.garcia@tec.mx>
2022-05-17 12:46:05 -0500

    Progressed but still incomplete

--- lib/hman_web/live/page_live.ex
+++ lib/hman_web/live/page_live.ex
@@ -4,16 +4,24 @@ defmodule HmanWeb.PageLive do
   @parts [:gallows, :head, :body, :rightArm, :leftArm, :rightLeg, :rightFoot, :leftLeg, :leftFoot]
 
   def mount(_params, _session, socket ) do
-    {:ok, socket}
+    {:ok, assign(socket, :feedback, "")}
   end
 
   def render(assigns) do
-    Phoenix.View.render(HmanWeb.PageView, "index.html", assign(assigns, :parts, []))
+    Phoenix.View.render(HmanWeb.PageView, "index.html", assign(assigns, :parts, [:gallows]))
+  end
+
+  def handle_event("new-game-req", _value, socket) do
+    {:ok, game} = Hman.Game.start_link(UUID.uuid1() |> String.to_atom)
+    {_turns, feedback} = Hman.Game.get_feedback(game)
+    {:noreply, socket |> assign(:game, game) |> assign(:feedback, feedback)}
   end
 
   def handle_event(event, _value, socket) do
     IO.inspect(event)
-    {:noreply, socket}
+    Hman.Game.submit_guess(socket.assigns.game, event)
+    {_turns, feedback} = Hman.Game.get_feedback(socket.assigns.game)
+    {:noreply, socket |> assign(:feedback, feedback)}
   end
 
 end
